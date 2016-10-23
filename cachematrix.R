75727efbd5bb854ccd6df9977c2f19a57df81b14
Xu <XUJ1@cleanharbors.com>
2016-10-23 17:27:08 -0400

    completed functions -- sophie

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+# 1. set the value of the vector
+		m <- NULL
 
+# 2. get the value of the vector
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+		
+# 3. set the value of the inverse
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+		
+# 4. get the value of the inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
@@ -12,4 +29,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
